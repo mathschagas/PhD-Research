8fc51a54e8b674c2866a44a959fda1b32ec4fa8f
Matheus Chagas <matheus.chagas@aluno.uece.br>
2024-11-04 19:34:29 -0300

    Fixing Quote Function
    Not finished yet

--- back/examples/components/template/quote_functions.py
+++ back/examples/components/template/quote_functions.py
@@ -1,13 +1,19 @@
 import math
 import hashlib
+import json
 from flask import jsonify
+
+with open('examples/components/template/quote_data.json', 'r') as file:
+    QUOTE_DATA = json.load(file)
 
 # Function to generate fixed values with better distribution based on component ID
 def generate_fixed_values(component_id, min_value, max_value):
+    if min_value == max_value:
+        return min_value
     # Generate a SHA-256 hash of the component_id
     hash_value = hashlib.sha256(component_id.encode('utf-8')).hexdigest()
-    # Convert a portion of the hash (first 8 characters) to an integer
-    hash_int = int(hash_value[:8], 16)
+    # Convert hash to an integer
+    hash_int = int(hash_value, 16)
     # Map the hash value to the desired range
     return min_value + (hash_int % (max_value - min_value))
 
@@ -153,6 +159,32 @@ def quote_scooter(lat1, lon1, lat2, lon2, component_id):
             "price": price,
             "safe_to_rain": generate_fixed_values(component_id, 0, 1),
             "secure_container": generate_fixed_values(component_id, 0, 1)
+        }
+    )
+
+
+def quote(component_type, lat1, lon1, lat2, lon2, component_id):
+    if component_type not in quote_functions:
+        return jsonify(
+            status="ERROR",
+            message="Component type not found."
+        )
+
+    speed = generate_fixed_values(component_id, QUOTE_DATA[component_type]['speed_min'], QUOTE_DATA[component_type]['speed_max'])
+    cost_per_km = generate_fixed_values(component_id, QUOTE_DATA[component_type]['cost_per_km_min'], QUOTE_DATA[component_type]['cost_per_km_max'])
+    distance = calculate_haversine_distance(lat1, lon1, lat2, lon2)
+    distance_to_package = generate_fixed_values(component_id, QUOTE_DATA[component_type]['distance_to_package_min'], QUOTE_DATA[component_type]['distance_to_package_max'])
+    time_hours = (distance + distance_to_package) / speed
+    time_to_deliver = round(time_hours * 60, 2)
+    price = round(distance * cost_per_km, 2)
+
+    return jsonify(
+        status="OK",
+        cbr={
+            "time_to_deliver": time_to_deliver,
+            "price": price,
+            "safe_to_rain": generate_fixed_values(component_id, QUOTE_DATA[component_type]['safe_to_rain_min'], QUOTE_DATA[component_type]['safe_to_rain_max']),
+            "secure_container": generate_fixed_values(component_id, QUOTE_DATA[component_type]['secure_container_min'], QUOTE_DATA[component_type]['secure_container_max'])
         }
     )
 
